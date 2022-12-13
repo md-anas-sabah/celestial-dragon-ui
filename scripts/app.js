6b431583df419148b11a025302fffaa9d1946059
Md Anas Sabah <mdanassabah@gmail.com>
2022-12-14 02:27:45 +0530

    landing page complete

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+function handleCopyTextFromParagraph() {
+  const paragraph = document.getElementById("clipboard-area");
+  paragraph.appendChild(area);
+
+  area.value = paragraph.innerText;
+  area.select();
+  document.execCommand("copy");
+
+  paragraph.removeChild(area);
+}
